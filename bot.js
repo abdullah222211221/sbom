6711cc3411a17cbe860561fa714dd0ddbf8d6876
abdullah222211221 <44670477+abdullah222211221@users.noreply.github.com>
2018-11-09 06:41:33 +0400

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,3 +1,41 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+ 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+ 
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = '.'
+ 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
+ 
+ 
 var prefix = "-"
 client.on('message', message => {
   if (message.author.x5bz) return;
@@ -37,6 +75,5 @@ client.on('message', message => {
   })
 }
 });
-
  
 client.login(process.env.BOT_TOKEN);
